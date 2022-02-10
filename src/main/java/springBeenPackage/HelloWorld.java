c2ee24537c297bc1b661107b6e96090d8aaf3d73
Hossein Javooni <h.javooni@gmail.com>
2022-02-10 03:41:09 +0330

    first_step_14001121_0341

--- src/main/java/springBeenPackage/HelloWorld.java
+++ src/main/java/springBeenPackage/HelloWorld.java
@@ -1,3 +1,5 @@
+package springBeenPackage;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import springBeenPackage.Hossein;
 import springBeenPackage.Person;
@@ -7,6 +9,7 @@ public class HelloWorld {
         System.out.println("Hi GitHub and java");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Hossein hossein = context.getBean("Hossein", Hossein.class);
+
         System.out.println(hossein.getFirstName());
     }
 }
