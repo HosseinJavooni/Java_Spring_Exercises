463052a15ea9c81840b03e8151f625bece6d751b
Hossein Javooni <h.javooni@faraboom.co>
2022-03-12 10:54:24 +0330

    Test_Lombok_Builder_10:54

--- src/main/java/lombok/MainLombok.java
+++ src/main/java/lombok/MainLombok.java
@@ -4,6 +4,8 @@ public class MainLombok {
     public static void main(String[] args) {
         Car car = new Car("QUIK", "saipa", 4);
         System.out.println(car.toString());
+        car = Car.builder().countOfWheel(4).name("HosseinJavooni").factory("Saipa").build();
+        System.out.println(car);
 
     }
 }
